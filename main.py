8eca771549300816db81e247266341022fa8d3f8
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-05-26 20:22:42 -0300

    Paso 1: Crear variables
    Creamos las variables necesarias para registrar la puntuación

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Primera Actividad Módulo 3: Proyecto - Piedra-Papel-Tijeras
+Versión: 0.1.0
+-------------------------------------
+REGLAS:
+Las reglas son sencillas: Los jugadores eligen simultáneamente uno de los tres signos: piedra, papel o tijera. El ganador se determina según las siguientes reglas: 
+1.    El papel le gana a la piedra (“El papel envuelve a la piedra”).
+2.    La piedra le gana a la tijera (“La piedra destruye a la tijera”).
+3.    La tijera le gana al papel (“La tijera corta el papel)
+Paso Nº 1: Creamos variables: 
+    > contadoras: ronda_actual; puntaje_jugador, puntaje_compu y empates;
+"""
+
+""" ··· [ Variables ] ··· """
+puntaje_jugador = 0
+puntaje_compu = 0
+empates = 0
+
+ronda_actual = 0
